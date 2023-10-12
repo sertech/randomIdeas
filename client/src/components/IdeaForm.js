668c561cf42d9cf891dfa8bb873891e8d4f3f0d4
sergio alvarez pinto <sertech_live@hotmail.com>
2023-10-12 18:10:36 -0400

    257-258 modal and ideaform components

---
+++ client/src/components/IdeaForm.js
@@ -0,0 +1,63 @@
+class IdeaForm {
+    constructor() {
+        this._formModal = document.querySelector('#form-modal');
+    }
+
+    addEventListeners() {
+        this._form.addEventListener('submit', this.handleSubmit.bind(this));
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        // in this._form.elements.text.value, text is the name of the element
+        const idea = {
+            text: this._form.elements.text.value,
+            tag: this._form.elements.tag.value,
+            username: this._form.elements.username.value,
+        };
+
+        console.log(idea);
+
+        // clear fields
+        this._form.elements.text.value = '';
+        this._form.elements.tag.value = '';
+        this._form.elements.username.value = '';
+
+        // since the modal and the form are separate elements
+        // to be able to close the modal from this component
+        // we have to dispatch a custom event which is a method of the DOM
+        // close modal is just its name can be anything
+        document.dispatchEvent(new Event('closemodal'));
+    }
+
+    render() {
+        this._formModal.innerHTML = `
+        <form id="idea-form">
+            <div class="form-control">
+                <label for="idea-text">Enter a Username</label>
+                <input type="text" name="username" id="username" />
+            </div>
+            <div class="form-control">
+                <label for="idea-text">What's Your Idea?</label>
+                <textarea name="text" id="idea-text"></textarea>
+            </div>
+            <div class="form-control">
+                <label for="tag">Tag</label>
+                <input type="text" name="tag" id="tag" />
+            </div>
+            <button class="btn" type="submit" id="submit">
+                Submit
+            </button>
+        </form>
+        `;
+
+        // we have to use the querySelector here because after
+        // this._formModal.innerHTML=`...` the #idea-form will exist
+        // so we can select it and attach the event listener
+        this._form = document.querySelector('#idea-form');
+        this.addEventListeners();
+    }
+}
+
+export default IdeaForm;
--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,9 @@
 // this is the entry point for the app
 import '@fortawesome/fontawesome-free/css/all.css';
+import Modal from './components/Modal';
+import IdeaForm from './components/IdeaForm';
 import './css/style.css';
+
+const modal = new Modal();
+const ideaForm = new IdeaForm();
+ideaForm.render();
