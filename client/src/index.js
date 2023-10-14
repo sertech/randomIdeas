4dedd8d25b4aa23b81dcd82c965452ed69850abe
sergio alvarez pinto <sertech_live@hotmail.com>
2023-10-14 02:41:37 -0400

    259 IdeaList Component

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,15 @@
 import '@fortawesome/fontawesome-free/css/all.css';
 import Modal from './components/Modal';
 import IdeaForm from './components/IdeaForm';
+import IdeaList from './components/IdeaList';
 import './css/style.css';
 
 const modal = new Modal();
+
+// initialize the ideaForm
 const ideaForm = new IdeaForm();
 ideaForm.render();
+
+// initialize the ideaList
+const ideaList = new IdeaList();
+ideaList.render();
