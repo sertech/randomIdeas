40b584ebc6526af5d794553c8c632711a3fb8ec1
sergio alvarez pinto <sertech_live@hotmail.com>
2023-10-15 19:50:23 -0400

    263 add delete functionality

--- client/src/components/IdeaList.js
+++ client/src/components/IdeaList.js
@@ -13,6 +13,19 @@ class IdeaList {
         this._validTags.add('inventions');
     }
 
+    addEventListeners() {
+        // we use delegation, where we put the click event in the entire list
+        // but we target the icon
+        this._ideaListEl.addEventListener('click', (e) => {
+            if (e.target.classList.contains('fa-times')) {
+                e.stopImmediatePropagation(); // event stays in the x icon not bubble up
+                const ideaId = e.target.parentElement.parentElement.dataset.id; // the actual card
+                console.log(ideaId);
+                this.deleteIdea(ideaId);
+            }
+        });
+    }
+
     // is async because axios returns a promise
     async getIdeas() {
         try {
@@ -23,6 +36,21 @@ class IdeaList {
             this.render(); // we render here to update the DOM
         } catch (error) {
             console.log(error);
+        }
+    }
+
+    async deleteIdea(ideaId) {
+        try {
+            // delete idea from server
+            const res = await IdeasApi.deleteIdea(ideaId);
+
+            // delete it from the DOM
+            this._ideas.filter((idea) => {
+                idea._id !== ideaId;
+            });
+            this.getIdeas();
+        } catch (error) {
+            alert('you can not delete this resource');
         }
     }
 
@@ -46,9 +74,13 @@ class IdeaList {
         this._ideaListEl.innerHTML = this._ideas
             .map((idea) => {
                 const tagClass = this.getTagClass(idea.tag);
+                const deleteBtn =
+                    idea.username === localStorage.getItem('username')
+                        ? `<button class="delete"><i class="fas fa-times"></i></button>`
+                        : '';
                 return `
-                <div class="card">
-                    <button class="delete"><i class="fas fa-times"></i></button>
+                <div class="card" data-id="${idea._id}">
+                    ${deleteBtn}
                     <h3>
                         ${idea.text}
                     </h3>
@@ -61,6 +93,9 @@ class IdeaList {
             `;
             })
             .join('');
+
+        // addListeners is here because the element inner HTML will exists after this point
+        this.addEventListeners();
     }
 }
 
